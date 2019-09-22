c3c97c4d56c2780a979d08c9759a1caaf1c5cfec
Kerem Bozdas <krmbzds.github@gmail.com>
2019-09-22 16:06:36 +0300

    Refactor code
    - Get rid of deprecated method by getting rid of ActiveSupport
    - Move glyphs to their locales = single pass transliteration
    - Use i18n gem and load transliterations automatically

--- lib/tate.rb
+++ lib/tate.rb
@@ -1,28 +1,16 @@
 lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
 $LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 
-require "tate/version"
+require 'tate/version'
 
 require 'i18n'
 require 'yaml'
 
+I18n.load_path << Dir[File.expand_path("lib/config/locales/*.yml")]
+I18n.enforce_available_locales = false
+
 module Tate
   def self.transliterate(string, language = nil, replacement = "?".freeze)
-    string = language_filter(string, language)
-    I18n.transliterate(ActiveSupport::Multibyte::Unicode.normalize(
-      ActiveSupport::Multibyte::Unicode.tidy_bytes(string), :c),
-        :replacement => replacement)
-  end
-
-  def self.language_filter(string, language)
-    glyph_rules = load_rules('glyphs')
-    language_rules = load_rules(language)
-    conversion_map = glyph_rules.merge(language_rules)
-    re = Regexp.new(conversion_map.keys.map { |x| Regexp.escape(x) }.join('|'))
-    string.gsub(re, conversion_map)
-  end
-
-  def self.load_rules(language)
-    YAML.load_file("lib/rules/#{language}.yml") rescue {}
+    I18n.transliterate(string, locale: language)
   end
 end
